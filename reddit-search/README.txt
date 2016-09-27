ae82e8261ef3b9f8e64ddd03005501fa0c8a2a90
Felipe Dominguez <felips.110@gmail.com>
2016-09-27 13:25:26 -0700

    This is the reddit Search app

---
+++ reddit-search/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/philgetz/pen/d60846c6e58e77760f112432fd2f3825.
+
+ 
---
+++ reddit-search/js/index.js
@@ -0,0 +1,46 @@
+function redditRetrieve(sub) {
+  var promise = $.ajax({
+    url: "https://www.reddit.com/r/" + sub + ".json",
+    type: 'get'
+  })
+  return promise
+}
+
+function callSubreddit(subreddit) {
+  redditRetrieve(subreddit).then(function(r) {
+
+    var requiredData = r.data.children.map(function(each) {
+      var archivedStatus = each.data.archived ? "Archived" : "Not Archived";
+      var requiredDataObject = {
+        author: each.data.author,
+        comments: each.data.num_comments,
+        title: each.data.title,
+        url: each.data.url,
+        archivedStatus: archivedStatus
+      }
+
+      return requiredDataObject
+    });
+    var templateSource = $('#reddit-list-template').html();
+    var template = Handlebars.compile(templateSource);
+    var html = template(requiredData);
+    $('.reddit-list').html(html);
+    $('.reddit-list').clear()
+
+  }, function() {
+    $('.reddit-list').html("<h2>No such sub-reddit, please search again</h2>");
+  })
+}
+
+
+$('#button').click(function() {
+  var subreddit = $('#box').val().toLowerCase();
+  callSubreddit(subreddit);
+})
+
+$('#box').keydown(function(e){
+  if(e.which === 13){
+   $('#button').click();
+    return false
+  }
+})
